567d346830bcb2a0b90302b497f2d9707590589d
Hunter Loftis <hunter@hunterloftis.com>
2015-09-16 00:56:07 -0700

    extract db

---
+++ db.js
@@ -0,0 +1,12 @@
+module.exports = {
+  1: [
+    { rating: 5, comment: "Great product!" },
+    { rating: 2, comment: "Strap broke :(" }
+  ],
+  2: [],
+  3: [
+    { rating: 1, comment: 'Smells funny' },
+    { rating: 2, comment: 'Too big' },
+    { rating: 3, comment: 'Okay bag' }
+  ]
+};
--- reviews.js
+++ reviews.js
@@ -1,16 +1,5 @@
 var jackrabbit = require('jackrabbit');
-var db = {
-  1: [
-    { rating: 5, comment: "Great product!" },
-    { rating: 2, comment: "Strap broke :(" }
-  ],
-  2: [],
-  3: [
-    { rating: 1, comment: 'Smells funny' },
-    { rating: 2, comment: 'Too big' },
-    { rating: 3, comment: 'Okay bag' }
-  ]
-}
+var db = require('./db');
 
 var rabbit = jackrabbit(process.env.CLOUDAMQP_URL);
 var exchange = rabbit.default();
